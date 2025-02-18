a4632f725e90189abdfb635977bbe9e70b1b32d1
Krishanu Saha <krishanu1137@gmail.com>
2025-02-18 22:58:07 +0530

    DAY2: Error Handling

--- main.go
+++ main.go
@@ -10,7 +10,14 @@ func main() {
 	// modules.GetTime()
 	// modules.Variables();
 	// modules.UserInput();
-	fmt.Println("Addition:", modules.Add(2,5));
-	fmt.Println("Subtraction:", modules.Subtract(2,4));
-	fmt.Println("Multiplication:", modules.Multiply(2,4));
+	// fmt.Println("Addition:", modules.Add(2,5));
+	// fmt.Println("Subtraction:", modules.Subtract(2,4));
+	// fmt.Println("Multiplication:", modules.Multiply(2,4));
+    division, err := modules.Division(10, 3)
+
+	if(err == nil) {
+		fmt.Println("Division:", division)
+	} else {
+		fmt.Println("Error:", err)
+	}
 }
---
+++ modules/ErrorHandle.go
@@ -0,0 +1,11 @@
+package modules
+
+import "errors"
+
+func Division(x, y float64) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("Cannot divide by zero")
+	} else {
+		return x / y, nil
+	}
+}
