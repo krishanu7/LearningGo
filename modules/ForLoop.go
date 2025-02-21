ca490381d7d3709a6ad0fa0ad0f2cb435b90279f
Krishanu Saha <krishanu1137@gmail.com>
2025-02-21 22:36:58 +0530

    For Loop as While Loop

--- modules/ForLoop.go
+++ modules/ForLoop.go
@@ -2,15 +2,23 @@ package modules
 
 import (
 	"fmt"
-)	
+)
 
 func LearnForLoop() {
 	// for i:=0; i<5; i++ {
 	// 	fmt.Println("Value of i:", i);
 	// }
-	arr := []int{14,21,34,45,56};
+	arr := []int{14, 21, 34, 45, 56}
 
 	for index, value := range arr {
-		fmt.Println("Index:", index, "Value:", value);
+		fmt.Println("Index:", index, "Value:", value)
+	}
+	var i = 0
+	for {
+		fmt.Println("Value of i:", i)
+		i++
+		if i == 5 {
+			break
+		}
 	}
 }
