3fa25728b2bac40d33f5429b68ab1df9bff8ede0
Krishanu Saha <krishanu1137@gmail.com>
2025-02-25 00:01:36 +0530

    Graceful Shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/krishanu7/students-api/internal/config"
 )
@@ -26,8 +32,32 @@ func main() {
 	}
 
 	fmt.Println("Starting server on", cfg.Addr)
-	err := server.ListenAndServe()
+	slog.Info("Starting server on", slog.String("addr", cfg.Addr))
+	// Create Channel to listen for OS signals
+	signalChan := make(chan os.Signal, 1);
+
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start server
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server:", err)
+		}
+	}()
+
+	// Wait for OS signal
+	<-signalChan
+
+	slog.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+
 	if err != nil {
-		log.Fatal("Failed to start server:", err)
+		slog.Error("Failed to shutdown server:", slog.String("error", err.Error()))
 	}
+	slog.Info("Server shutdown successfully")
 }
